e80a52cff0ad2321853888a9b6a25b6e7bb6c03b
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-04 15:12:19 +0700

    feat: configuring navigation if authenticated and unauthenticated

--- frontend-project/src/components/Navbar.tsx
+++ frontend-project/src/components/Navbar.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import { useAuthStore } from "../store/userAuth"
 
 const Navbar = () => {
+
+    const { token } = useAuthStore()
+
     return (
         <header className="flex justify-between py-5 items-center z-50 max-w-5xl mx-auto w-full">
             <div>
@@ -8,13 +12,35 @@ const Navbar = () => {
             </div>
             <div>
                 <ul className="flex gap-x-[40px] text-sm font-bold mr-10">
-                    <li><Link to={"/"} className="text-[#737373]">Home</Link></li>
-                    <li><Link to={"/login"} className="text-white">Login</Link></li>
-                    <li><Link to={"/register"} className="underline text-white">Register</Link></li>
-                    <li></li>
+                    {
+                        token !== "" ? <NavAuth /> : <NavUnauth />
+                    }
                 </ul>
             </div>
         </header>
+    )
+}
+
+const NavUnauth = () => {
+    return (
+        <>
+            <li><Link to={"/"} className="text-[#737373]">Home</Link></li>
+            <li><Link to={"/login"} className="text-white">Login</Link></li>
+            <li><Link to={"/register"} className="underline text-white">Register</Link></li>
+            <li></li>
+        </>
+    )
+}
+
+const NavAuth = () => {
+    return (
+        <>
+            <li><Link to={"/"} className="text-[#737373]">Home</Link></li>
+            <li><Link to={"/login"} className="text-[#737373]">Transfer</Link></li>
+            <li><Link to={"/topup"} className="text-[#737373]">Topup</Link></li>
+            <li><Link to={"/games"} className="text-[#737373]">Games</Link></li>
+            <li><Link to={"/logout"} className="text-[#737373]">Logout</Link></li>
+        </>
     )
 }
 
