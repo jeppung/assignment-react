5770699e38cbcfc069553ea106a09f7d0dec0685
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-04 11:16:12 +0700

    feat: configuring react router route, adding pages

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Topup from './pages/Topup'
+import Transfer from './pages/Transfer'
+import Games from './pages/Games'
+import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 
 function App() {
 
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='/topup' element={<Topup />} />
+        <Route path='/transfer' element={<Transfer />} />
+        <Route path='/games' element={<Games />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ frontend-project/src/pages/PageNotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className='flex flex-col justify-center items-center'>
+                <h1>404</h1>
+                <h2>Page Not Found</h2>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound
