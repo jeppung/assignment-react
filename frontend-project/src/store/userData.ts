15b18b9a36c11cd450307ad701182eaf8f310525
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-04 19:34:44 +0700

    feat: modifying store to be persistent

--- frontend-project/src/store/userData.ts
+++ frontend-project/src/store/userData.ts
@@ -1,15 +1,21 @@
 
 import { create } from 'zustand'
 import { IUserDataResponse } from '../interfaces/apiResponse'
+import { persist } from 'zustand/middleware'
+
 
 interface IUserDataStore {
     userData: Partial<IUserDataResponse>,
     setUserData: (data: IUserDataResponse) => void
 }
 
-export const useUserDataStore = create<IUserDataStore>((set) => (
-    {
-        userData: {},
-        setUserData: (data) => set(() => ({ userData: data }))
-    }
-))
+export const useUserDataStore = create(
+    persist<IUserDataStore>((set) => (
+        {
+            userData: {},
+            setUserData: (data) => set(() => ({ userData: data }))
+        }
+    ), {
+        name: "userData"
+    })
+)
