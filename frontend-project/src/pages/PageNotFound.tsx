3bb48eb0a2c2d2c4d269db2448bc80b83ad08c34
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-05 21:50:32 +0700

    feat: modifying page not found with Lottie

--- frontend-project/src/pages/PageNotFound.tsx
+++ frontend-project/src/pages/PageNotFound.tsx
@@ -1,9 +1,13 @@
+import Lottie from "lottie-react";
+
+import notFoundAnimation from "../assets/404.json"
+
 const PageNotFound = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='flex flex-col justify-center items-center'>
-                <h1>404</h1>
-                <h2>Page Not Found</h2>
+                <Lottie animationData={notFoundAnimation} loop={true} />
+                <h1 className="text-3xl font-bold">Page Not Found</h1>
             </div>
         </div>
     )
