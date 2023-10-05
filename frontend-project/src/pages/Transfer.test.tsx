ac65a244abefe8380ee6a6e630b464a4e86b4a95
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-05 21:15:09 +0700

    feat: add input test

--- frontend-project/src/pages/Transfer.test.tsx
+++ frontend-project/src/pages/Transfer.test.tsx
@@ -2,6 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import Transfer from './Transfer';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
+import { useState } from 'react';
+import InputAmount from '../components/InputAmount';
+
+const MockInputAmount = () => {
+    const [value, setValue] = useState<number | string>("")
+
+    return <InputAmount onChange={(data) => setValue(data)} value={value} />
+}
 
 describe("Transfer Page", () => {
     it("Should render transfer form", async () => {
@@ -14,5 +22,27 @@ describe("Transfer Page", () => {
         const transferForm = await screen.findByTestId("transfer-form")
 
         expect(transferForm).toBeVisible()
+    })
+
+    it("Input amount can be typed", async () => {
+        const user = userEvent.setup()
+        render(<MockInputAmount />)
+
+        const inputAmount = await screen.findByLabelText("Amount")
+
+        await user.type(inputAmount, "123")
+
+        expect(inputAmount).toHaveValue(123)
+    })
+
+    it("Input amount cannot be typed with alphabet characters", async () => {
+        const user = userEvent.setup()
+        render(<MockInputAmount />)
+
+        const inputAmount = await screen.findByLabelText("Amount")
+
+        await user.type(inputAmount, "abc")
+
+        expect(inputAmount).not.toHaveValue("abc")
     })
 })
