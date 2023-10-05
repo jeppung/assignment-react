d91e7d5ae3d67f6dc93782ce13a2e4701025d7c9
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-05 15:41:44 +0700

    feat: removing unused hook

--- frontend-project/src/store/transactionsStore.ts
+++ frontend-project/src/store/transactionsStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { ITopupResponse, ITransactionsResponse } from "../interfaces/apiResponse";
+import { ITransactionsResponse } from "../interfaces/apiResponse";
 import { persist } from "zustand/middleware";
 
 interface ITransactionsStore {
