4ac79e8af20e00c54a6b0b2168a79e7d4b0b14e4
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-05 21:29:58 +0700

    feat: add input placeholder for Transfer and Topup

--- frontend-project/src/components/InputAmount.tsx
+++ frontend-project/src/components/InputAmount.tsx
@@ -8,7 +8,7 @@ const InputAmount = ({ onChange, value }: IInputAmountProps) => {
     return (
         <div className="flex flex-col gap-y-3">
             <label htmlFor="amount" className="font-bold">Amount</label>
-            <input onChange={(e) => onChange(parseInt(e.target.value))} value={value} type="number" name="amount" id="amount" className="border-[1px] border-[#4F4F4F] rounded-md px-5 py-3 text-xl h-[72px]" required />
+            <input onChange={(e) => onChange(parseInt(e.target.value))} value={value} placeholder="1000000" type="number" name="amount" id="amount" className="border-[1px] border-[#4F4F4F] rounded-md px-5 py-3 text-xl h-[72px]" required />
         </div>
     )
 }
